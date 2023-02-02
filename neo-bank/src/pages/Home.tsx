a87fb2e7160f5e84c5df86c7626b89bf0c5b4da2
Alexandr <vih_64@mail.ru>
2023-02-02 18:07:08 +0400

    fixed

--- neo-bank/src/pages/Home.tsx
+++ neo-bank/src/pages/Home.tsx
@@ -11,7 +11,6 @@ import {
 } from "../components";
 import { api } from "api/api";
 import { CURRENCY } from "constant";
-import Wrapper from "./Wrapper";
 
 const Home: FC = () => {
     const [currency, setCurrency] = useState([]);
@@ -43,39 +42,25 @@ const Home: FC = () => {
     }, []);
 
     return (
-        <Wrapper>
-            <ChooseCreditCard />
+        <div className="wrapper">
+            <Header />
+            <main>
+                <div className="wrapper__main">
+                    <ChooseCreditCard />
 
-            <FunctionsBank />
+                    <FunctionsBank />
 
-            <ExchangeRates currency={currency} />
+                    <ExchangeRates currency={currency} />
 
-            <BankMap />
+                    <BankMap />
 
-            <SliderNews news={news} />
+                    <SliderNews news={news} />
 
-            <SubscribeNewsSupport />
-        </Wrapper>
-
-        // <div className="wrapper">
-        //     <Header />
-        //     <main>
-        //         <div className="wrapper__main">
-        //             <ChooseCreditCard />
-
-        //             <FunctionsBank />
-
-        //             <ExchangeRates currency={currency} />
-
-        //             <BankMap />
-
-        //             <SliderNews news={news} />
-
-        //             <SubscribeNewsSupport />
-        //         </div>
-        //     </main>
-        //     <Footer />
-        // </div>
+                    <SubscribeNewsSupport />
+                </div>
+            </main>
+            <Footer />
+        </div>
     );
 };
 
