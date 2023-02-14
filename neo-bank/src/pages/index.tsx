bad70256dfa70ba2e6b182f00ee3f8365afdb113
Alexandr <vih_64@mail.ru>
2023-02-14 13:23:18 +0400

    fix style

--- neo-bank/src/pages/index.tsx
+++ neo-bank/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export { Home } from './Home';
 export { Loan } from './Loan';
-export { LoanApplication } from './LoanStep/LoanApplication';
-export { LoanDocument } from './LoanStep/LoanDocument';
-export { LoanDocumentSign } from './LoanStep/LoanDocumentSign';
-export { LoanCode } from './LoanStep/LoanCode';
+export { LoanApplication } from './LoanStep/LoanApplication/LoanApplication';
+export { LoanDocument } from './LoanStep/LoanDocument/LoanDocument';
+export { LoanDocumentSign } from './LoanStep/LoanDocumentSign/LoanDocumentSign';
+export { LoanCode } from './LoanStep/LoanCode/LoanCode';
 export { NotFound } from './NotFound';
